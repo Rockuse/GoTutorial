fe8d931ac8d7d9b8c44c445793ab874bd73bb352
Fahmi Muzakki <fahmirockman@gmail.com>
2022-04-14 07:38:21 +0700

    new file:   Variable.go 	new file:   go.mod 	new file:   go.work 	new file:   hello.go

---
+++ Variable.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var eko string="eko"
+
+func main() {
+	fmt.Println(eko)
+}
---
+++ type-declaration.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+type noKTP string
+var noKTPEko noKTP= "tes"
+
+func main() {
+
+	fmt.Println("tes")
+}
